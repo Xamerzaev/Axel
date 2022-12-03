2990364ce1f64ca621e498cc3c3a86d1591566ac
Ramzan <ramzanxam@gmail.com>
2022-12-03 13:49:09 +0300

    errors

--- index/views.py
+++ index/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import redirect, render
 from django.core.mail import EmailMultiAlternatives
+from django.http import HttpResponse
+from django.contrib import messages
 from geopy.geocoders import Nominatim
-import polyline
 import folium
 from route.getroute import get_route
 
@@ -38,26 +39,33 @@ def index(request):
         return redirect('index')
 
     elif request.method == 'POST' and 'btn2' in request.POST:
-        from_country = request.POST.get("from_country")
-        from_city = request.POST.get("from_city")
-        from_street = request.POST.get('from_street')
-        to_country = request.POST.get("to_country")
-        to_city = request.POST.get("to_city")
-        to_street = request.POST.get('to_street')
-        app = Nominatim(user_agent='test')
-        from_location = app.geocode(f"{from_street},{from_city}, {from_country}").raw
-        to_location = app.geocode(f"{to_street},{to_city}, {to_country}").raw
-        # print raw data
-        figure = folium.Figure()
-        lat1,long1,lat2,long2=float(from_location['lat']),float(from_location['lon']),float(to_location['lat']),float(to_location['lon'])
-        route = get_route(long1,lat1,long2,lat2)
-        m = folium.Map(location=[(route['start_point'][0]),
-                                    (route['start_point'][1])], 
-                        zoom_start=10)
-        m.add_to(figure)
-        folium.PolyLine(route['route'],weight=8,color='blue',opacity=0.6).add_to(m)
-        figure.render()
-        context={'map':figure}
-        return render(request,'route/showroute.html',context)
+        if HttpResponse.status_code == 200:
+            from_country = request.POST.get("from_country")
+            from_city = request.POST.get("from_city")
+            from_street = request.POST.get('from_street')
+            to_country = request.POST.get("to_country")
+            to_city = request.POST.get("to_city")
+            to_street = request.POST.get('to_street')
+            app = Nominatim(user_agent='test')
+            try:
+                from_location = app.geocode(f"{from_street},{from_city}, {from_country}").raw
+                to_location = app.geocode(f"{to_street},{to_city}, {to_country}").raw
+                # print raw data
+                figure = folium.Figure()
+                lat1,long1,lat2,long2=float(from_location['lat']),float(from_location['lon']),float(to_location['lat']),float(to_location['lon'])
+                route = get_route(long1,lat1,long2,lat2)
+                m = folium.Map(location=[(route['start_point'][0]),
+                                            (route['start_point'][1])], 
+                                zoom_start=10)
+                m.add_to(figure)
+                folium.PolyLine(route['route'],weight=8,color='blue',opacity=0.6).add_to(m)
+                figure.render()
+                context={'map':figure}
+                return render(request,'route/showroute.html',context)
+            except:
+                messages.error(request, 'Введите правильные данные')
+                return render(request, 'index.html')
+        else:
+            return render(request, 'index.html')
 
     return render(request, 'index.html')
--- test.py
+++
@@ -1,5 +0,0 @@
-from geopy.geocoders import Nominatim
-app = Nominatim(user_agent='test')
-location = app.geocode("Хаджиева 53,Грозный, Россия").raw
-# print raw data
-print(location)
